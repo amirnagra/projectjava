27280632c813df35fa160907b196deb453ed6918
aa_na <aa_na@LAPTOP-V7KTPFVG>
2019-02-18 00:04:40 -0500

    suits enum with a property for card class

---
+++ JavaApplication19/src/javaapplication19/Card.java
@@ -0,0 +1,19 @@
+
+
+package javaapplication19;
+
+/**
+ *
+ * @author aa_na
+ */
+public class Card {
+   public enum Suits {
+       RED,
+       GREEN,
+       YELLOW,
+       BLUE
+   }
+   
+   public Suits suit;
+
+}
